9f2309d0af151361e7686ee9b5fdc1103aae3891
Devesh Kumar <devesh2027@gmail.com>
2021-05-03 15:28:47 +0530

    Initialized Block Class

---
+++ Block.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+#include "./Transaction.cpp"
+
+class Block {
+    std::string prevHash;
+    Transaction transaction;
+    float timeStamp;
+
+   public:
+    Block(std::string prevHash, Transaction transaction, float timeStamp) {
+        this->timeStamp = timeStamp;
+        this->transaction = transaction;
+        this->prevHash = prevHash;
+    }
+};
