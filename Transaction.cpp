7f1693d97c07761311899d49ece029d280c26a05
Devesh Kumar <devesh2027@gmail.com>
2021-05-03 15:28:39 +0530

    Updated 'this' pointer in transaction class.

--- Transaction.cpp
+++ Transaction.cpp
@@ -8,26 +8,26 @@ class Transaction {
 
     public:
         float getAmount(){
-            return this.amount;
+            return this->amount;
         }
 
         std::string getPayer(){
-            return this.payer;
+            return this->payer;
         }
 
         std::string getPayee(){
-            return this.payee;
+            return this->payee;
         }
 
         Transaction(std::string payer, std::string payee, float amount){
-            this.amount = amount;
-            this.payer = payer;
-            this.payee = payee;
+            this->amount = amount;
+            this->payer = payer;
+            this->payee = payee;
         }
 
 		Transaction() {
-			this.amount = 0;
-			this.payer = "";
-			this.payee = "";
+			this->amount = 0;
+			this->payer = "";
+			this->payee = "";
 		}
 }
